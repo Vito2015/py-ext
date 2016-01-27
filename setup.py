634c271d33b8f2aa04241954f3dd128e21f691db
Vito2015 <w_yeer9988@126.com>
2016-01-27 15:45:21 +0800

    change package name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 """
-    py-ext.setup
+    setup
     ~~~~~~~~~~~~
-    py-ext package setup
+    py_ext package setup
 
     :copyright: (c) 2016 by Vito.
     :license: GNU, see LICENSE for more details.
@@ -11,13 +11,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='py-ext',
+    name='py_ext',
     version='1.0',
     description='python extension package',
     author='Vito',
     author_email='vito2015@live.com',
-    url='https://github.com/Vito2015/py-ext',
-    download_url='https://github.com/Vito2015/py-ext.git',
+    url='https://github.com/Vito2015/py_ext',
+    download_url='https://github.com/Vito2015/py_ext.git',
     license='GNU',
     packages=find_packages(),
 
