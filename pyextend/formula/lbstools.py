91ab406209164d66f35e06a346b51e7b4725b8a8
Vito2008 <w_yeer9988@126.com>
2016-04-25 23:46:00 +0800

    add cacl_distance method in formula.lbstools

--- pyextend/formula/lbstools.py
+++ pyextend/formula/lbstools.py
@@ -11,7 +11,7 @@ import math
 
 
 def haversine(lng1, lat1, lng2, lat2):
-    """Compute km by geo-coordinate
+    """Compute km by geo-coordinates
     See also: haversine define https://en.wikipedia.org/wiki/Haversine_formula
     """
     # Convert coordinates to floats.
@@ -27,3 +27,28 @@ def haversine(lng1, lat1, lng2, lat2):
     c = 2 * math.asin(math.sqrt(a))
     km = 6367 * c
     return km
+
+
+def calc_distance(lng1, lat1, lng2, lat2):
+    """Calc distance (km) by geo-coordinates.
+        @:param lng1: first coordinate.lng
+        @:param lat1: first coordinate.lat
+        @:param lng2: second coordinate.lng
+        @:param lat2: second coordinate.lat
+        @:return distance: km
+    """
+    ra = 6378.140  # 赤道半径 (km)
+    rb = 6356.755  # 极半径 (km)
+    flatten = (ra - rb) / ra  # 地球扁率
+    rad_lat_1 = math.radians(lat1)
+    rad_lng_1 = math.radians(lng1)
+    rad_lat_2 = math.radians(lat2)
+    rad_lng_2 = math.radians(lng2)
+    p1 = math.atan(rb / ra * math.tan(rad_lat_1))
+    p2 = math.atan(rb / ra * math.tan(rad_lat_2))
+    xx = math.acos(math.sin(p1) * math.sin(p2) + math.cos(p1) * math.cos(p2) * math.cos(rad_lng_1 - rad_lng_2))
+    c1 = (math.sin(xx) - xx) * (math.sin(p1) + math.sin(p2)) ** 2 / math.cos(xx / 2) ** 2
+    c2 = (math.sin(xx) + xx) * (math.sin(p1) - math.sin(p2)) ** 2 / math.sin(xx / 2) ** 2
+    dr = flatten / 8 * (c1 - c2)
+    distance = ra * (xx + dr)
+    return distance
--- tests/test_formula_lbstools.py
+++ tests/test_formula_lbstools.py
@@ -19,5 +19,15 @@ def test_haversine():
 
     assert haversine(bj_lng, bj_lat, tj_lng, tj_lat) == bj_tj_km
 
+
+def test_calc_distance():
+    from pyextend.formula.lbstools import calc_distance
+
+    bj_lat, bj_lng = 39.8904172019, 116.3919153781
+    tj_lat, tj_lng = 39.0769187666, 117.1943166228
+    bj_tj_km = 113.67822015376474
+
+    assert calc_distance(bj_lng, bj_lat, tj_lng, tj_lat) == bj_tj_km
+
 if __name__ == '__main__':
     pytest.main()
